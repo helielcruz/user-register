4cb734863b83a5eb6c4f24ad5e782a632b52d4d6
helielcruz <helielcruz@gmail.com>
2023-10-07 01:38:11 -0300

    refactor: add redirect in same page

--- frontend/src/services/currentUser.js
+++ frontend/src/services/currentUser.js
@@ -11,11 +11,11 @@ document.addEventListener('DOMContentLoaded', async () => {
             document.getElementById('title').innerHTML = "Bem vindo "+data.name
             console.log(data);
         }else {
-            window.open('http://localhost/user-register/')
+            window.open('http://localhost/user-register/', '_self')
         }
         
     } catch (error) {
-        window.open('http://localhost/user-register/')
+        window.open('http://localhost/user-register/', '_self')
         console.error(error);
     }
     
